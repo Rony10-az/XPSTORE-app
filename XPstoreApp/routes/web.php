5b2a0bc93841d41b16b062ca0171163e48cb150b
Rony <ronnjhost@gmail.com>
2025-11-11 08:23:33 -0500

    comentario

--- XPstoreApp/routes/web.php
+++ XPstoreApp/routes/web.php
@@ -12,3 +12,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+#comentario
