281261aa5c72dfdf03c802d4826f3b291494ad7b
oscarjscom <oscar.olano@tecsup.edu.pe>
2025-11-22 14:05:43 -0500

    se solucion el inicio a creacion de videojuego

--- XPstoreApp/resources/views/admin/videojuegos/create.blade.php
+++ XPstoreApp/resources/views/admin/videojuegos/create.blade.php
@@ -12,14 +12,14 @@
             <h1><i class="fas fa-plus"></i> Crear Nuevo Videojuego</h1>
             <p class="admin-subtitle">Agrega un nuevo juego al catálogo de XP Store</p>
         </div>
-        <a href="{{ route('admin.videojuegos.index') }}" class="btn-secondary">
+        <a href="{{ route('videojuegos.index') }}" class="btn-secondary">
             <i class="fas fa-arrow-left"></i> Volver al Listado
         </a>
     </div>
 
     {{-- Formulario --}}
     <div class="form-container">
-        <form action="{{ route('admin.videojuegos.store') }}" method="POST" enctype="multipart/form-data" class="crud-form">
+        <form action="{{ route('videojuegos.store') }}" method="POST" enctype="multipart/form-data" class="crud-form">
             @csrf
 
             {{-- Información Básica --}}
